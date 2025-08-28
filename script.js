cd1f16436e937abdc7bbd752cce26da56904603c
Evgeny Khatskov <45400445+YVM2L@users.noreply.github.com.>
2025-08-28 10:19:40 +0300

    added function calculate; result.toString

--- script.js
+++ script.js
@@ -8,24 +8,56 @@ updateDisplay("0"); // начальное значение
 
 const buttons = document.querySelectorAll("button");
 
-let firstOperand = ''; // первое число
-let operator = ''; // знак
-let secondOperand = ''; // второе число
+let firstOperand = ""; // первое число
+let operator = ""; // знак
+let secondOperand = ""; // второе число
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
+function calculate() {
+  const a = parseFloat(firstOperand);
+  const b = parseFloat(secondOperand);
+  let result;
+
+  switch (
+    operator //проверка какой оператор выбран
+  ) {
+    case "+":
+      result = a + b;
+      break;
+    case "-":
+      result = a - b;
+      break;
+    case "*":
+      result = a * b;
+      break;
+    case "/":
+      result = b !== 0 ? a / b : "Ошибка"; // на ноль не делим
+      break;
+  }
+
+  updateDisplay(result);
+  firstOperand = result.toString(); //результат превращается в новое первое число для продолжения новых вычислений
+  secondOperand = ""; // сброс второго числа
+  operator = ""; // сброс оператора
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
     const value = button.textContent;
 
     if (!isNaN(value)) {
-      if (!operator) { // первое число
+      if (!operator) {
+        // первое число
         firstOperand += value;
         updateDisplay(firstOperand);
-      } else { // второе число
+      } else {
+        // второе число
         secondOperand += value;
         updateDisplay(secondOperand);
       }
+    } else if (value === "=") {
+      calculate(); //если = , то вызывается функция calculate
     } else {
-      operator = value; //знак
+      operator = value;
     }
   });
 });
