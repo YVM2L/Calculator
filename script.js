a0002c184273304c25b222ea37884338db157a8f
Evgeny Khatskov <45400445+YVM2L@users.noreply.github.com.>
2025-08-28 09:44:23 +0300

    variables added; can enter numbers, select an operator, enter a second number

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-const display = document.getElementById('display');
+const display = document.getElementById("display");
 
 function updateDisplay(value) {
   display.textContent = value;
 }
 
-updateDisplay('0'); // начальное значение
+updateDisplay("0"); // начальное значение
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll("button");
+
+let firstOperand = ''; // первое число
+let operator = ''; // знак
+let secondOperand = ''; // второе число
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     const value = button.textContent;
-    updateDisplay(value); // вывод на экран
+
+    if (!isNaN(value)) {
+      if (!operator) { // первое число
+        firstOperand += value;
+        updateDisplay(firstOperand);
+      } else { // второе число
+        secondOperand += value;
+        updateDisplay(secondOperand);
+      }
+    } else {
+      operator = value; //знак
+    }
   });
 });
